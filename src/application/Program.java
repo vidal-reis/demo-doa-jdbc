9ae79e876ee3d8f23a1a68ef44873fcedc7b077d
Gustavo Vidal <reis.vidal21@gmail.com>
2020-05-14 20:23:51 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,11 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 4: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Upadate Completed");
 	}
 	
 
